433da161269bbb3e695fa60e10707ae710454c26
Diego Abreu <diego.abreu@matera.com>
2016-04-27 08:28:28 -0300

    Atualizando fragment das comissoes

--- app/src/main/java/com/diegoabreu/pocketsenado/fragment/ComissoesFragment.java
+++ app/src/main/java/com/diegoabreu/pocketsenado/fragment/ComissoesFragment.java
@@ -9,22 +9,16 @@ import android.view.ViewGroup;
 
 import com.diegoabreu.pocketsenado.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ComissoesFragment extends Fragment {
 
-
-    public ComissoesFragment() {
-        // Required empty public constructor
-    }
-
+    View myView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comissoes, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fragment_comissoes, container, false);
+        setHasOptionsMenu(true);
+
+        return myView;
     }
 
 }
